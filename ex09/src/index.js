a54c9d5edc098ed763d26d8b024d305393dc30dc
LadiFlow <adi_curt@hotmail.com>
2021-03-16 19:19:16 +0100

    Introducing else statemant

---
+++ ex09/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function elseFunction(num) {
+    if (num <= 5) {
+        return "5 or smaller";
+    }   else {
+        return "Bigger than 5";
+    }
+    }
+    console.log(elseFunction(4));
+    console.log(elseFunction(5));
+    console.log(elseFunction(6));
+    console.log(elseFunction(10));
+    
+// Only change code above this line
+module.exports = elseFunction;
