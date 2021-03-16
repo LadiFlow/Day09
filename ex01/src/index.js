e499658347d8ee81790947128116b70f6dd5cd99
LadiFlow <adi_curt@hotmail.com>
2021-03-16 15:16:15 +0100

    Boolean values

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+function myTrueBollean() {
+    return true;
+}
+console.log(myTrueBollean());
+// Only change code above this line
+module.exports = myTrueBollean;
