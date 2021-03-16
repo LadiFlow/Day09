8fef530dfde7db5ec37d1998c758ba4b21b65b9d
LadiFlow <adi_curt@hotmail.com>
2021-03-16 16:04:27 +0100

    Equality and strict equality operators

---
+++ ex03/src/index.js
@@ -0,0 +1,23 @@
+// Only change code below this line
+function myEqualFunction(num) {
+    if (num === 23) {
+        return "Equal";
+    }   else {
+        return "Not equal";
+    }
+      
+    }
+
+    (myEqualFunction(5));
+    (myEqualFunction(23));
+    (myEqualFunction("23"));
+    (myEqualFunction('23'));
+    (myEqualFunction("text"));
+
+console.log(myEqualFunction(5));
+console.log(myEqualFunction(23));
+console.log(myEqualFunction("23"));
+console.log(myEqualFunction('23'));
+console.log(myEqualFunction("text"));
+// Only change code above this line
+module.exports = myEqualFunction;
