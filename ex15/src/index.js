767fb9b0bacdfe3a008cfa4d5937fbab4b5c103d
LadiFlow <adi_curt@hotmail.com>
2021-03-17 01:24:22 +0100

    Do while loops

---
+++ ex15/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+function myDoWhile(){
+var myNumbers=(0);
+do {
+   console.log(myNumbers);
+   myNumbers+=1;
+} while (myNumbers < 10) + ",";
+return myNumbers;
+}
+(myDoWhile());
+
+// Only change code above this line
+module.exports = myDoWhile;
